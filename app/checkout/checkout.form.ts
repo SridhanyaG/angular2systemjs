9a736b4ec7fd1b39e55ec0a8319ecc64b1a57a07
SridhanyaG <sridhanya77@gmail.com>
2016-07-24 18:38:41 +0530

    adding changes to form

--- app/checkout/checkout.form.ts
+++ app/checkout/checkout.form.ts
@@ -1,9 +1,31 @@
 import {Component} from '@angular/core';
-
+import {Order} from './checkout.order';
 @Component({
     selector:'checkout',
-    template:'checkout content'
+    templateUrl:'app/checkout/checkout.form.html',
+    styles:[
+        `
+        .control-label.required::after {
+            content:"*";
+            color:red;
+        }
+        .ng-valid[required] {
+        border-left: 5px solid #42A948; /* green */
+        }
+
+        .ng-invalid {
+        border-left: 5px solid #a94442; /* red */
+        }
+        `
+    ]
 })
 export class CheckOutForm {
+    order:Order;
+    constructor(){
+        this.order = new Order();
+    }
 
+logForm(value: any) {
+    console.log(value);
+  }
 }
---
+++ app/checkout/checkout.order.ts
@@ -0,0 +1,10 @@
+export class Order {
+    firstName:string;
+    lastName:string;
+    streetAddress:string;
+    zipcode:number;
+    city:string;
+    state:string;
+    phone:number;
+    emailAddress:string;
+}
